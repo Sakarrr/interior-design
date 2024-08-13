5c421c3d066c20fcb5a6da348b899e32e2b97703
Sakarrr <ctha.sakar@gmail.com>
2024-08-13 20:38:06 +0545

    Initial Commit

---
+++ main.php
@@ -0,0 +1,126 @@
+<div class="int-main">
+    <div class="container">
+        <div class="row">
+            <div class="int-main-header">
+                <h1 class="int-main-heading">Interior Design</h1>
+
+                <div class="int-showcase">
+                    <h2> Showcase. </h2>
+                    <div class="int-showcase-grid">
+                        <img src="https://www.w3schools.com/w3images/kitchenconcrete.jpg"></img>
+                        <img src="https://www.w3schools.com/w3images/atrium.jpg"></img>
+                        <img src="https://www.w3schools.com/w3images/livingroom.jpg"></img>
+                        <img src="https://www.w3schools.com/w3images/bedroom.jpg"></img>
+                        <img src="https://www.w3schools.com/w3images/diningroom.jpg"></img>
+                        <img src="https://www.w3schools.com/w3images/livingroom2.jpg"></img>
+                    </div>
+                </div>
+                <div class="int-services">
+                    <h2> Services. </h2>
+                    <p>
+                        We are a interior design service that focus on what's best for your home and what's best for you!
+                    </p>
+
+                    <p>
+                        Some text about our services - what we do and what we offer. We are lorem ipsum consectetur
+                        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                        mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        abore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                <div class="int-designers">
+                    <h2> Designers. </h2>
+
+                    <p>
+                        The best team in the world.
+                    </p>
+
+                    <p>
+                        We are lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat.
+                    </p>
+
+                    <span>Our designers are thoughtfully chosen:</span>
+
+                    <div class="int-designers-list">
+                        <div class="int-designer-card">
+                            <img src="https://www.w3schools.com/w3images/team2.jpg">
+                            <div class="card-content">
+                                <h3>John Doe</h3>
+                                <span>CEO & Founder</span>
+                                <p>
+                                    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="int-designer-card">
+                            <img src="https://www.w3schools.com/w3images/team2.jpg">
+                            <div class="card-content">
+                                <h3>John Doe</h3>
+                                <span>CEO & Founder</span>
+                                <p>
+                                    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="int-designer-card">
+                            <img src="https://www.w3schools.com/w3images/team2.jpg">
+                            <div class="card-content">
+                                <h3>John Doe</h3>
+                                <span>CEO & Founder</span>
+                                <p>
+                                    Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="int-packages">
+                    <h2>Packages.</h2>
+                    <p>
+                        Some text our prices. Lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                    </p>
+                    <div class="packages-list">
+                        <div class="package-list">
+                            <table>
+                                <tr>
+                                    <th>Basic</th>
+                                </tr>
+                                <tr>
+                                    <td>Floorplanning</td>
+                                </tr>
+                                <tr>
+                                    <td>10 hours support</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="package-list pro">
+                            <table>
+                                <tr>
+                                    <th>Pro</th>
+                                </tr>
+                                <tr>
+                                    <td>Floorplanning</td>
+                                </tr>
+                                <tr>
+                                    <td>50 hours support</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ sidebar.php
@@ -0,0 +1,20 @@
+<div class ="int-sidebar">
+    <div class="container">
+        <div class="row">
+            <div class="int-sidebar-header">
+                Company
+                Name
+            </div>
+            <div class="int-sidebar-menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Showcase</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Designers</a></li>
+                    <li><a href="#">Packages</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>    
+</div>
