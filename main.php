2fc97161505ef053c156a4f4fedc21c82246a602
Sakarrr <ctha.sakar@gmail.com>
2024-08-13 20:45:33 +0545

    Add - Contact form

--- main.php
+++ main.php
@@ -120,6 +120,23 @@
                         </div>
                     </div>
                 </div>
+                <div class="int-contact">
+                    <h2>Contact.</h2>
+                    <p>
+                        Do you want us to style your home? Fill out the form and fill me in with the details :) We love meeting new people!
+                    </p>
+                    <div class="contact-form">
+                        <form>
+                            <label>Name</label>
+                            <input type="text">
+                            <label>Email</label>
+                            <input type="text">
+                            <label>Message</label>
+                            <input type="text">
+                            <input type="submit" value="Send Message">
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
