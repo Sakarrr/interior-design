a91acad4bab0a39156fdbdc3f415eb1c90a4eb39
Sakarrr <ctha.sakar@gmail.com>
2024-08-14 11:18:21 +0545

    Add - Ratio height for images & break point variables

--- showcase.php
+++ showcase.php
@@ -4,12 +4,12 @@
             <h1 class="interior-heading">Interior Design</h1>
             <h2 class="interior-showcase-heading"> Showcase. </h2>
             <div class="interior-showcase-photogrid">
-                <a href="#"><img src="https://www.w3schools.com/w3images/kitchenconcrete.jpg"></img></a>
-                <a href="#"><img src="https://www.w3schools.com/w3images/atrium.jpg"></img></a>
-                <a href="#"><img src="https://www.w3schools.com/w3images/livingroom.jpg"></img></a>
-                <a href="#"><img src="https://www.w3schools.com/w3images/bedroom.jpg"></img></a>
+                <a href="#"><img src="https://images.pexels.com/photos/27702855/pexels-photo-27702855/free-photo-of-amigo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></a>
+                <a href="#"><img src="https://images.pexels.com/photos/27647759/pexels-photo-27647759/free-photo-of-a-person-holding-a-candle-in-the-dark.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></a>
+                <a href="#"><img src="https://images.pexels.com/photos/745081/pexels-photo-745081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></a>
+                <a href="#"><img src="https://images.pexels.com/photos/27000784/pexels-photo-27000784/free-photo-of-three-men-are-pulling-a-net-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></a>
                 <a href="#"><img src="https://www.w3schools.com/w3images/diningroom.jpg"></img></a>
-                <a href="#"><img src="https://www.w3schools.com/w3images/livingroom2.jpg"></img></a>
+                <a href="#"><img src="https://images.pexels.com/photos/27054232/pexels-photo-27054232/free-photo-of-a-road-with-a-sign-that-says-road-to-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></a>
             </div>
         </div>
     </div>
