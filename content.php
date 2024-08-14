5d9f8abf7fb035c3515310ca801694d285191768
Sakarrr <ctha.sakar@gmail.com>
2024-08-14 13:42:46 +0545

    Fix - Base container and sidebar padding issue

--- content.php
+++ content.php
@@ -1,10 +1,8 @@
 <div class="interior-main">
-    <div class="container">
-        <?php include './sections/showcase.php' ?>
-        <?php include './sections/services.php' ?>
-        <?php include './sections/designers.php' ?>
-        <?php include './sections/packages.php' ?>
-        <?php include './sections/contact.php' ?>
-        <?php include './sections/footer.php' ?>
-    </div>
+    <?php include './sections/showcase.php' ?>
+    <?php include './sections/services.php' ?>
+    <?php include './sections/designers.php' ?>
+    <?php include './sections/packages.php' ?>
+    <?php include './sections/contact.php' ?>
+    <?php include './sections/footer.php' ?>
 </div>
--- sections/showcase.php
+++ sections/showcase.php
@@ -1,10 +1,10 @@
 <div class="interior-showcase" id="interior-showcase">
     <div class="container">
+        <h1 class="interior-heading">
+            Interior Design
+        </h1>
+        <h2 class="interior-showcase-heading"> Showcase. </h2>
         <div class="row">
-            <h1 class="interior-heading">
-                Interior Design
-            </h1>
-            <h2 class="interior-showcase-heading"> Showcase. </h2>
             <div class="interior-showcase-photogrid">
                 <a data-fslightbox href="https://images.pexels.com/photos/27702855/pexels-photo-27702855/free-photo-of-amigo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"><img src="https://images.pexels.com/photos/27702855/pexels-photo-27702855/free-photo-of-amigo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></a>
                 <a data-fslightbox href="https://images.pexels.com/photos/27647759/pexels-photo-27647759/free-photo-of-a-person-holding-a-candle-in-the-dark.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"><img src="https://images.pexels.com/photos/27647759/pexels-photo-27647759/free-photo-of-a-person-holding-a-candle-in-the-dark.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img></a>
