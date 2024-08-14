0e66197b284eab2bd388777eab1b9b916f31811f
Sakarrr <ctha.sakar@gmail.com>
2024-08-14 17:55:16 +0545

    Add - Float view css

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
 <div class="initial-overlay"></div>
 
 <!-- <body class="flex-box"> -->
-<body class="grid-view">
-<!-- <body class="float"> -->
+<!-- <body class="grid-view"> -->
+<body class="float">
 
     <?php include 'sidebar.php'; ?>
     <div class=" overlay" id="overlay">
