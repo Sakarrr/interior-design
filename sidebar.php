b52d441dbfe458461b66f149433c1be5411f42d3
Sakarrr <ctha.sakar@gmail.com>
2024-08-14 13:33:42 +0545

    Fix - Row div content on sidebar

--- sidebar.php
+++ sidebar.php
@@ -13,23 +13,21 @@
 
 <div class="interior-sidebar">
     <div class="container">
-        <div class="row">
-            <div class="interior-sidebar-close" id="interior-sidebar-close">
-                <span>Close Menu</span>
-            </div>
-            <div class="interior-sidebar-header">
-                <b>Company Name</b>
-            </div>
-            <div class="interior-sidebar-menu">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#interior-showcase">Showcase</a></li>
-                    <li><a href="#interior-services">Services</a></li>
-                    <li><a href="#interior-designers">Designers</a></li>
-                    <li><a href="#interior-packages">Packages</a></li>
-                    <li><a href="#interior-contact">Contact</a></li>
-                </ul>
-            </div>
+        <div class="interior-sidebar-close" id="interior-sidebar-close">
+            <span>Close Menu</span>
+        </div>
+        <div class="interior-sidebar-header">
+            <b>Company Name</b>
+        </div>
+        <div class="interior-sidebar-menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#interior-showcase">Showcase</a></li>
+                <li><a href="#interior-services">Services</a></li>
+                <li><a href="#interior-designers">Designers</a></li>
+                <li><a href="#interior-packages">Packages</a></li>
+                <li><a href="#interior-contact">Contact</a></li>
+            </ul>
         </div>
     </div>
 </div>
