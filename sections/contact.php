a9f2613334f61782c59a27a808edfa7f6ea9cae0
Sakarrr <ctha.sakar@gmail.com>
2024-08-14 13:49:28 +0545

    Fix - Contact section .row issue

--- sections/contact.php
+++ sections/contact.php
@@ -1,27 +1,25 @@
 <div class="interior-contact" id="interior-contact">
     <div class="container">
-        <div class="row">
-            <h2>Contact.</h2>
-            <p>
-                Do you want us to style your home? Fill out the form and fill me in with the details :) We love meeting new people!
-            </p>
-            <div class="contact-form">
-                <form>
-                    <div class="name-container">
-                        <label>Name</label>
-                        <input type="text">
-                    </div>
-                    <div class="email-container">
-                        <label>Email</label>
-                        <input type="text">
-                    </div>
-                    <div class="message-container">
-                        <label>Message</label>
-                        <input type="text">
-                    </div>
-                    <button type="submit">Send Message</button>
-                </form>
-            </div>
+        <h2>Contact.</h2>
+        <p>
+            Do you want us to style your home? Fill out the form and fill me in with the details :) We love meeting new people!
+        </p>
+        <div class="contact-form">
+            <form>
+                <div class="name-container">
+                    <label>Name</label>
+                    <input type="text">
+                </div>
+                <div class="email-container">
+                    <label>Email</label>
+                    <input type="text">
+                </div>
+                <div class="message-container">
+                    <label>Message</label>
+                    <input type="text">
+                </div>
+                <button type="submit">Send Message</button>
+            </form>
         </div>
     </div>
 </div>
