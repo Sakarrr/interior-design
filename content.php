7b3620e4719cc722dafeecd7b920d62fd560623a
Sakarrr <ctha.sakar@gmail.com>
2024-08-15 11:24:51 +0545

    Implement - dynamic services section

--- content.php
+++ content.php
@@ -1,9 +1,11 @@
 <div class="interior-main">
     <?php include './sections/showcase.php' ?>
-    <?php echo showcase('row', 'col col-lg-6'); 
+    <?php echo showcase('row', 'col col-lg-6');
     ?>
-    <?php //echo showcase('grid grid-cols-2', ''); ?>
+    <?php //echo showcase('grid grid-cols-2', ''); 
+    ?>
     <?php include './sections/services.php' ?>
+    <?php echo services(); ?>
     <?php include './sections/designers.php' ?>
     <?php include './sections/packages.php' ?>
     <?php include './sections/contact.php' ?>
