cf9c94af66c3d4e948d87e6f087d0b5cef376ce9
Sakarrr <ctha.sakar@gmail.com>
2024-08-15 12:19:51 +0545

    Implement - dynamic packages section and refactor styles

--- content.php
+++ content.php
@@ -1,16 +1,30 @@
 <div class="interior-main">
-    <?php include './sections/showcase.php' ?>
-    <?php echo showcase('row', 'col col-lg-6');
+    <?php
+    include './sections/showcase.php';
+    echo showcase('row', 'col col-lg-6');
+    //echo showcase('grid grid-cols-2', '');
     ?>
-    <?php //echo showcase('grid grid-cols-2', ''); 
+
+    <?php
+    include './sections/services.php';
+    echo services();
     ?>
-    <?php include './sections/services.php' ?>
-    <?php echo services(); ?>
+
     <?php
     include './sections/designers.php';
     echo designers('grid grid-cols-3', '');
     ?>
-    <?php include './sections/packages.php' ?>
-    <?php include './sections/contact.php' ?>
-    <?php include './sections/footer.php' ?>
+
+    <?php
+    include './sections/packages.php';
+    echo packages('grid grid-cols-2', '');
+    ?>
+
+    <?php
+    include './sections/contact.php';
+    ?>
+
+    <?php
+    include './sections/footer.php';
+    ?>
 </div>
--- sections/packages.php
+++ sections/packages.php
@@ -1,72 +1,80 @@
-<div class="interior-packages" id="interior-packages">
-    <div class="container">
+<?php
+function packages($row, $col)
+{
+?>
+    <div class="interior-packages" id="interior-packages">
+        <div class="container">
 
-        <h2>Packages.</h2>
-        <p>
-            Some text our prices. Lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure
-        </p>
-        <div class="row packages-list clearfix">
-            <div class="package-list col col-lg-6">
-                <ul>
-                    <li>
-                        Basic
-                    </li>
-                    <li>
-                        FloorPlanning
-                    </li>
-                    <li>
-                        10 hours support
-                    </li>
-                    <li>
-                        Photography
-                    </li>
-                    <li>
-                        20% furniture discount
-                    </li>
-                    <li>
-                        Good deals
-                    </li>
-                    <li>
-                        <h3>$ 199</h3>
-                        <span>per room</span>
-                    </li>
-                    <li>
-                        <a href="#">Sign up</a>
-                    </li>
-                </ul>
-            </div>
+            <h2>Packages.</h2>
+            <p>
+                Some text our prices. Lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            </p>
+            <div class="packages-list">
+                <div class="<?php echo $row ?>">
+                    <div class="package-list <?php $col ?>">
+                        <ul>
+                            <li>
+                                Basic
+                            </li>
+                            <li>
+                                FloorPlanning
+                            </li>
+                            <li>
+                                10 hours support
+                            </li>
+                            <li>
+                                Photography
+                            </li>
+                            <li>
+                                20% furniture discount
+                            </li>
+                            <li>
+                                Good deals
+                            </li>
+                            <li>
+                                <h3>$ 199</h3>
+                                <span>per room</span>
+                            </li>
+                            <li>
+                                <a href="#">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
 
-            <div class="package-list col col-lg-6 pro">
-                <ul>
-                    <li class="pro">
-                        Pro
-                    </li>
-                    <li>
-                        FloorPlanning
-                    </li>
-                    <li>
-                        50 hours support
-                    </li>
-                    <li>
-                        Photography
-                    </li>
-                    <li>
-                        50% furniture discount
-                    </li>
-                    <li>
-                        GREAT deals
-                    </li>
-                    <li>
-                        <h3>$ 249</h3>
-                        <span>per room</span>
-                    </li>
-                    <li>
-                        <a href="#" class="pro">Sign up</a>
-                    </li>
-                </ul>
+                    <div class="package-list <?php $col ?> pro">
+                        <ul>
+                            <li class="pro">
+                                Pro
+                            </li>
+                            <li>
+                                FloorPlanning
+                            </li>
+                            <li>
+                                50 hours support
+                            </li>
+                            <li>
+                                Photography
+                            </li>
+                            <li>
+                                50% furniture discount
+                            </li>
+                            <li>
+                                GREAT deals
+                            </li>
+                            <li>
+                                <h3>$ 249</h3>
+                                <span>per room</span>
+                            </li>
+                            <li>
+                                <a href="#" class="pro">Sign up</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+<?php
+}
