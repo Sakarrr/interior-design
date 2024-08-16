422becfe6f1fad6f8b12f024bbd5d993cda16a2b
Sakarrr <ctha.sakar@gmail.com>
2024-08-16 14:14:26 +0545

    Add - jquery file

--- index.php
+++ index.php
@@ -84,6 +84,7 @@
     </div>
 
     <script src="./assets/js/main.js"></script>
+    <script src="./assets/js/jquery.js"></script>
     <script src="./assets/js/fslightbox.js"></script>
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 </body>
