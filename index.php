d2aa847e34d2b769be34168a185184b1937e791a
Sakarrr <ctha.sakar@gmail.com>
2024-08-16 14:18:36 +0545

    Add - dynamic stylesheets and js

--- index.php
+++ index.php
@@ -6,15 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flex - Interior Design</title>
 
-    <link rel="stylesheet" href="./assets/stylesheet/css/style.css" />
-
-    <!-- Include Poppins font. -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-    <!-- Include Ionicons. -->
-    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <?php include './stylesheets.php' ?>
 </head>
 
 <div class="loader-container">
@@ -83,10 +75,7 @@
         ?>
     </div>
 
-    <script src="./assets/js/main.js"></script>
-    <script src="./assets/js/jquery.js"></script>
-    <script src="./assets/js/fslightbox.js"></script>
-    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <?php include './scripts.php' ?>
 </body>
 
 </html>
