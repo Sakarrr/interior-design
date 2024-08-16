caa0260669e60f3a0f9580ba35117ba5707efcb2
Sakarrr <ctha.sakar@gmail.com>
2024-08-16 11:43:58 +0545

    Add - Sidebar menu icons

--- sidebar.php
+++ sidebar.php
@@ -8,12 +8,12 @@
             </div>
             <div class="interior-sidebar-menu">
                 <ul class="primary-menu">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#interior-showcase">Showcase</a></li>
-                    <li><a href="#interior-services">Services</a></li>
-                    <li><a href="#interior-designers">Designers</a></li>
-                    <li><a href="#interior-packages">Packages</a></li>
-                    <li><a href="#interior-contact">Contact</a></li>
+                    <li><a href="#">Home <ion-icon name="home"></ion-icon></a></li>
+                    <li><a href="#interior-showcase">Showcase <ion-icon name="images"></ion-icon></a></li>
+                    <li><a href="#interior-services">Services <ion-icon name="settings"></ion-icon></a></li>
+                    <li><a href="#interior-designers">Designers <ion-icon name="brush"></ion-icon></a></li>
+                    <li><a href="#interior-packages">Packages <ion-icon name="cube"></ion-icon></a></li>
+                    <li><a href="#interior-contact">Contact <ion-icon name="call"></ion-icon></a></li>
                 </ul>
             </div>
         </div>
