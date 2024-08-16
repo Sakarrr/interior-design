81c3dfaef60c6b3b4d0e436c41a518504699163c
Sakarrr <ctha.sakar@gmail.com>
2024-08-16 16:50:10 +0545

    Update - Footer text

--- sections/footer.php
+++ sections/footer.php
@@ -3,7 +3,7 @@ function footer()
 {
 ?>
     <div class="interior-footer">
-        <p>Powered by <a href="#">w3.css</a></p>
+        <p>Template by <a href="#">w3.css</a>. Enhanced by .</p>
     </div>
 
     <a class="interior-scroll-to-top">
