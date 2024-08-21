4e3e5d760effd5f3a58c411d85ca6c209e3cfb4c
sakarrr <ctha.sakar@gmail.com>
2024-08-21 17:28:27 +0545

    Remove - Dead files

--- scripts.php
+++ scripts.php
@@ -1,4 +1,3 @@
 <script src="./assets/js/main.js"></script>
-<script src="./assets/js/jquery.js"></script>
 <script src="./assets/js/fslightbox.js"></script>
 <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
