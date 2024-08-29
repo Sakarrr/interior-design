bd02a32e6acd825d4622c54fbdaa809bf894e8d9
sakarrr <ctha.sakar@gmail.com>
2024-08-29 16:54:35 +0545

    Add - Tool tip on sidebar menu

--- sidebar.php
+++ sidebar.php
@@ -8,12 +8,24 @@
             </div>
             <div class="id-sidebar-menu">
                 <ul class="primary-menu">
-                    <li class="id-fs-xl"><a href="#"><ion-icon name="home"></ion-icon><span>Home</span> </a></li>
-                    <li class="id-fs-xl"><a href="#interior-showcase"><ion-icon name="images"></ion-icon><span>Showcase</span> </a></li>
-                    <li class="id-fs-xl"><a href="#interior-services"><ion-icon name="settings"></ion-icon><span>Services</span> </a></li>
-                    <li class="id-fs-xl"><a href="#interior-designers"><ion-icon name="brush"></ion-icon><span>Designers</span> </a></li>
-                    <li class="id-fs-xl"><a href="#interior-packages"><ion-icon name="cube"></ion-icon><span>Packages</span> </a></li>
-                    <li class="id-fs-xl"><a href="#interior-contact"><ion-icon name="call"></ion-icon><span>Contact</span> </a></li>
+                    <li class="id-fs-xl"><a href="#"><ion-icon name="home"></ion-icon>
+                            <div class="id-tool-tip">Home</div><span class="id-fs-lg">Home</span>
+                        </a></li>
+                    <li class="id-fs-xl"><a href="#interior-showcase"><ion-icon name="images"></ion-icon>
+                            <div class="id-tool-tip">Showcase</div><span class="id-fs-lg">Showcase</span>
+                        </a></li>
+                    <li class="id-fs-xl"><a href="#interior-services"><ion-icon name="settings"></ion-icon>
+                            <div class="id-tool-tip">Services</div><span class="id-fs-lg">Services</span>
+                        </a></li>
+                    <li class="id-fs-xl"><a href="#interior-designers"><ion-icon name="brush"></ion-icon>
+                            <div class="id-tool-tip">Designers</div><span class="id-fs-lg">Designers</span>
+                        </a></li>
+                    <li class="id-fs-xl"><a href="#interior-packages"><ion-icon name="cube"></ion-icon>
+                            <div class="id-tool-tip">Packages</div><span class="id-fs-lg">Packages</span>
+                        </a></li>
+                    <li class="id-fs-xl"><a href="#interior-contact"><ion-icon name="call"></ion-icon>
+                            <div class="id-tool-tip">Contact</div><span class="id-fs-lg">Contact</span>
+                        </a></li>
                 </ul>
             </div>
         </div>
